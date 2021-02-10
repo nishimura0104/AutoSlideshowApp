82394557f71896f7f81384c028773dca35183904
nishimura0104 <rei.47.sh@icloud.com>
2021-02-10 20:36:49 +0900

    最初のコミット

---
+++ app/src/main/java/jp/techacademy/rei/nishimura/autoslideshowapp/MainActivity.kt
@@ -0,0 +1,93 @@
+package jp.techacademy.rei.nishimura.autoslideshowapp
+
+import android.Manifest
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.content.pm.PackageManager
+import android.os.Build
+import android.util.Log
+import android.provider.MediaStore
+import android.content.ContentUris
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private val PERMISSIONS_REQUEST_CODE = 100
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Android 6.0以降の場合
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            // パーミッションの許可状態を確認する
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                // 許可されている
+                getContentsInfo()
+            } else {
+                // 許可されていないので許可ダイアログを表示する
+                requestPermissions(
+                    arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                    PERMISSIONS_REQUEST_CODE
+                )
+            }
+            // Android 5系以下の場合
+        } else {
+            getContentsInfo()
+        }
+
+        back_button.setOnClickListener (this)
+        start_stop_button.setOnClickListener (this)
+        next_button.setOnClickListener (this)
+    }
+
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.back_button -> {}
+            R.id.start_stop_button -> {}
+            R.id.next_button -> {}
+        }
+     }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            PERMISSIONS_REQUEST_CODE ->
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    getContentsInfo()
+                }
+        }
+    }
+
+    private fun getContentsInfo() {
+        // 画像の情報を取得する
+        val resolver = contentResolver
+        val cursor = resolver.query(
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI, // データの種類
+            null, // 項目（null = 全項目）
+            null, // フィルタ条件（null = フィルタなし）
+            null, // フィルタ用パラメータ
+            null // ソート (nullソートなし）
+        )
+
+        if (cursor!!.moveToFirst()) {
+            do {
+                // indexからIDを取得し、そのIDから画像のURIを取得する
+                val fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID)
+                val id = cursor.getLong(fieldIndex)
+                val imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
+
+                Log.d("ANDROID", "URI : " + imageUri.toString())
+                imageView.setImageURI(imageUri)
+
+            } while (cursor.moveToNext())
+        }
+        cursor.close()
+
+
+    }
+}
